713a3462805e830bfa284570a5e74e824dddb251
Willem Dehaes <willemdehaes@gmail.com>
2016-07-04 19:13:46 +0000

    validation (27)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end  
   end
 
   def show
@@ -28,12 +32,17 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+    
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -41,7 +50,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
