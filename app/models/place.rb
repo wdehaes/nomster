65cdc61f5005a0d2f6c3a4e8c3be5c3c28b2d712
Willem Dehaes <willemdehaes@gmail.com>
2016-07-07 17:28:39 +0000

    comments in db (31)

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, presence: true
   validates :description, presence: true
